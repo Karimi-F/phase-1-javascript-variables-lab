a71f88b5d99e1fed4052ce621cb465bf9fb70f96
Karimi-F <fridah37karimi@gmail.com>
2024-09-24 08:13:26 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; // set companyName to Scuber
+let mostProfitableNeighborhood = 'Chelsea' // set the mostProfitable Neighbourhood to Chelsea
+let companyCeo = 'Susan Smith' // set companyCeo to Susan Smith
